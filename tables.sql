aa37269bf72f7ec98f4770804eeb10d21a887c0e
AymNmbrk <aymanmbarki48@gmail.com>
2022-09-14 16:36:43 +0200

    LETS GO

--- tables.sql
+++ tables.sql
@@ -1,17 +1,16 @@
-create database Music2;
-use Music2;
-
-CREATE TABLE playlist (ID INT AUTO_INCREMENT PRIMARY KEY, User TEXT, Title TEXT, Artist TEXT);
-
-INSERT INTO playlist (User, Title, Artist) VALUES ("maissae", "Bad Romance", "Lady Gaga");
-INSERT INTO playlist (User, Title, Artist) VALUES ("waleed", "Pink Venom", "Blackpink");
-INSERT INTO playlist (User, Title, Artist) VALUES ("khadija", "Umbrella", "Rihanna");
-INSERT INTO playlist (User, Title, Artist) VALUES ("aymane", "Voxel", "Raid");
 
 
-CREATE TABLE connexion (ID INT AUTO_INCREMENT PRIMARY KEY, User TEXT, Password TEXT);
+CREATE TABLE Music2.playlist (ID INT AUTO_INCREMENT PRIMARY KEY, User TEXT, Title TEXT, Artist TEXT);
 
-INSERT INTO connexion (User, Password) VALUES ("khadija", "khadija");
-INSERT INTO connexion (User, Password) VALUES ("aymane", "aymane");
-INSERT INTO connexion (User, Password) VALUES ("maissae", "maissae");
-INSERT INTO connexion (User, Password) VALUES ("waleed", "waleed");
+INSERT INTO Music2.playlist (User, Title, Artist) VALUES ("maissae", "Bad Romance", "Lady Gaga");
+INSERT INTO Music2.playlist (User, Title, Artist) VALUES ("waleed", "Pink Venom", "Blackpink");
+INSERT INTO Music2.playlist (User, Title, Artist) VALUES ("khadija", "Umbrella", "Rihanna");
+INSERT INTO Music2.playlist (User, Title, Artist) VALUES ("aymane", "Voxel", "Raid");
+
+
+CREATE TABLE Music2.connexion (ID INT AUTO_INCREMENT PRIMARY KEY, User TEXT, Password TEXT);
+
+INSERT INTO Music2.connexion (User, Password) VALUES ("khadija", "khadija");
+INSERT INTO Music2.connexion (User, Password) VALUES ("aymane", "aymane");
+INSERT INTO Music2.connexion (User, Password) VALUES ("maissae", "maissae");
+INSERT INTO Music2.connexion (User, Password) VALUES ("waleed", "waleed");
