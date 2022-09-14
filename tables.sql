f9026104bc18c7a37e01756d12f5e31a6daf0b71
AymNmbrk <aymanmbarki48@gmail.com>
2022-09-14 10:12:48 +0200

    LETS GO

--- tables.sql
+++ tables.sql
@@ -6,6 +6,7 @@ CREATE TABLE playlist (ID INT AUTO_INCREMENT PRIMARY KEY, User TEXT, Title TEXT,
 INSERT INTO playlist (User, Title, Artist) VALUES ("maissae", "Bad Romance", "Lady Gaga");
 INSERT INTO playlist (User, Title, Artist) VALUES ("waleed", "Pink Venom", "Blackpink");
 INSERT INTO playlist (User, Title, Artist) VALUES ("khadija", "Umbrella", "Rihanna");
+INSERT INTO playlist (User, Title, Artist) VALUES ("aymane", "Voxel", "Raid");
 
 
 CREATE TABLE connexion (ID INT AUTO_INCREMENT PRIMARY KEY, User TEXT, Password TEXT);
